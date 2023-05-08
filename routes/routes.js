02b833a8e4af2febd0296f8465dcc9a6f88785a2
a5hi5hx <ashishpaudel54@gmail.com>
2023-05-08 15:43:18 +0545

    retuyrn

--- routes/routes.js
+++ routes/routes.js
@@ -170,6 +170,13 @@ favorites:addToFavorites,
   }
 });
 
-
+router.get('/allitems', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
     module.exports = router;
